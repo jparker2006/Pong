a7fdf32abbfb588d87789b6eb455afeb90f35956
jparker2006 <jakeparkeremail@gmail.com>
2021-09-08 00:30:16 -0700

    paddles move

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+# ball should change velo after hitting top/bottom wall or paddle
+
 import pygame, random, sys
 from pygame.locals import *
 pygame.init()
@@ -6,7 +8,6 @@ resolution = (800,600)
 # colors
 c_white = (255, 255, 255)
 c_black = (0, 0, 0)
-c_maroon = (176, 48, 96)
 # colors end
 
 class Ball:
@@ -35,6 +36,26 @@ class Ball:
         self.dYVel = 2
         self.bStarted = True
 
+class Paddle:
+    def __init__(self, xPos, yPos = resolution[1] / 2 - 55, xVel = 0, yVel = 0):
+        self.nXPos = xPos
+        self.nYPos = yPos
+        self.dXVel = xVel
+        self.dYVel = yVel
+        self.bOnKey = False
+        self.type = "Paddle"
+    def draw(self, surface):
+        pygame.draw.rect(surface, c_white, pygame.Rect(self.nXPos, self.nYPos, 30, 110))
+    def update(self):
+        self.nXPos += self.dXVel
+        self.nYPos += self.dYVel
+    def MoveUp(self):
+        self.dYVel -= 1
+    def MoveDown(self):
+        self.dYVel += 1
+    def KeyUp(self):
+        self.bOnKey = False
+
 class MainGame():
     def __init__(self):
         pygame.init()
@@ -42,6 +63,8 @@ class MainGame():
         self.screen = pygame.display.set_mode(resolution)
         self.clock = pygame.time.Clock()
         self.Ball = Ball()
+        self.PaddleA = Paddle(10) # W and A
+        self.PaddleB = Paddle(resolution[0] - 40) # keyup and keydown
 
     def handleEvents(self):
         for event in pygame.event.get():
@@ -53,15 +76,27 @@ class MainGame():
                     self.Ball.start()
                 elif keys[K_ESCAPE]:
                     sys.exit()
+                elif keys[K_w]:
+                    self.PaddleA.MoveUp()
+                elif keys[K_s]:
+                    self.PaddleA.MoveDown()
+                elif keys[K_UP]:
+                    self.PaddleB.MoveUp()
+                elif keys[K_DOWN]:
+                    self.PaddleB.MoveDown()
 
     def run(self):
         while True:
             self.handleEvents()
             self.Ball.update()
+            self.PaddleA.update()
+            self.PaddleB.update()
 
             self.screen.fill(c_black)
 
             self.Ball.draw(self.screen)
+            self.PaddleA.draw(self.screen)
+            self.PaddleB.draw(self.screen)
 
             self.clock.tick(60) #regulate FPS
             pygame.display.flip()
