f3df55a99396ff83a1157580711dcd806258b446
jparker2006 <jakeparkeremail@gmail.com>
2021-09-07 20:01:17 -0700

    keyboard input and start of a jump

--- Main.py
+++ Main.py
@@ -1,29 +1,34 @@
-import pygame, sys, random
+import pygame, random
 from pygame.locals import *
 pygame.init()
 
 resolution = (500,500)
-white = (255, 255, 255)
-black = (0, 0, 0)
+# colors
+c_white = (255, 255, 255)
+c_black = (0, 0, 0)
+c_maroon = (176, 48, 96, 1)
+# colors end
 
-class Ball:
+class Dragon:
     # xPos, yPos, xVelo, yVelo, ballsize (constructor)
-    def __init__(self, xPos =  resolution[0] / 2, yPos = resolution[1] / 2, xVel = 0, yVel = 3, rad = 50):
-        self.x = xPos
-        self.y = yPos
-        self.dx = xVel
-        self.dy = yVel
-        self.radius = rad
-        self.type = "ball"
+    def __init__(self, xPos =  resolution[0] / 2, yPos = resolution[1] / 2, xVel = 0, yVel = 4.5, rad = 30):
+        self.nXPos = xPos
+        self.nYPos = yPos
+        self.dXVel = xVel
+        self.dYVel = yVel
+        self.nRadius = rad
+        self.type = "dragon"
     def draw(self, surface):
-        pygame.draw.circle(surface, black, (self.x, self.y), self.radius)
+        pygame.draw.circle(surface, c_black, (self.nXPos, self.nYPos), self.nRadius)
     def update(self):
-        self.x += self.dx
-        self.y += self.dy
-        if (self.x <= 0 or self.x >= resolution[0]): #bounce off walls
-            self.dx *= -1
-        if (self.y <= 50 or self.y >= resolution[1] - 50):
-            self.dy *= -1
+        self.nXPos += self.dXVel
+        self.nYPos += self.dYVel
+        if (self.nXPos <= 30 or self.nXPos >= resolution[0] - 30): #bounce off walls
+            self.dXVel *= -1
+        if (self.nYPos <= 30 or self.nYPos >= resolution[1] - 30):
+            self.dYVel *= -1
+    def jump(self):
+        self.dYVel *= -1
 
 class MainGame():
     def __init__(self):
@@ -31,30 +36,33 @@ class MainGame():
         self.setTitle = pygame.display.set_caption('Dragon Jumper')
         self.screen = pygame.display.set_mode(resolution)
         self.clock = pygame.time.Clock()
-        self.gameObjects = []
-        self.gameObjects.append(Ball())
+        self.Dragon = Dragon()
 
     def handleEvents(self):
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
-            elif (event.type == pygame.KEYDOWN) or (event.type == pygame.MOUSEBUTTONDOWN):
-                print("PRESS")
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.Dragon.jump()
+            elif event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[K_UP] or keys[K_SPACE] or keys[K_w]:
+                    self.Dragon.jump()
+                elif keys[K_ESCAPE]:
+                    pygame.quit()
 
     def run(self):
         while True:
             self.handleEvents()
+            self.Dragon.update()
 
-            for gameObj in self.gameObjects:
-                gameObj.update()
+            self.screen.fill(c_maroon)
 
-            self.screen.fill(white)
-
-            for gameObj in self.gameObjects:
-                gameObj.draw(self.screen)
+            self.Dragon.draw(self.screen)
 
             self.clock.tick(60) #regulate FPS
             pygame.display.flip()
+
 
 MainGame().run()
 
